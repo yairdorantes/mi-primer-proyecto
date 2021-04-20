71c77fa9baf33944407c14146051829a86261f80
yair prz <yairmasterlol@gmail.com>
2021-04-19 22:33:22 -0500

    topo

--- webs/settings.py
+++ webs/settings.py
@@ -148,7 +148,7 @@ EMAIL_HOST_PASSWORD='xrl8xdgta'
 #STATIC_URL = '/static/'
 STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL='/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),os.path.join(BASE_DIR,'media'))
+STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 django_heroku.settings(locals())
 STATIC_FILES = (
