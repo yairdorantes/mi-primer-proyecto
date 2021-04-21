b5432f1548eab73ea53745b7f981d5668bf69caa
yair prz <yairmasterlol@gmail.com>
2021-04-21 11:43:50 -0500

    r

--- webs/settings.py
+++ webs/settings.py
@@ -148,7 +148,7 @@ EMAIL_HOST='smtp.gmail.com'
 EMAIL_USE_TLS=True
 EMAIL_PORT=587
 EMAIL_HOST_USER='companyseller.ml@gmail.com'
-EMAIL_HOST_PASSWORD='hpukonftbifnulkw'
+EMAIL_HOST_PASSWORD='kqjmbxvjndeewvsr'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
