1acb79340f6d41b5d3aafd55520263765d147b76
yair prz <yairmasterlol@gmail.com>
2021-04-22 22:04:23 -0500

    n

--- webs/settings.py
+++ webs/settings.py
@@ -96,21 +96,6 @@ DATABASES = {
 
 
 '''
-import dj_database_url
-from decouple import config
-DATABASES = {
-    'default':dj_database_url.config(
-        default=config('DATABASE_URL')
-    )
-}
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-'''
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
